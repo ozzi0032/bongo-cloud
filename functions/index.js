86be2209e18b3780bbe691de25ab843fcaf91d3c
Mohammad Uzair <muzair3284@outlook.com>
2020-08-12 22:47:18 +0500

    web API's hosting

--- functions/index.js
+++ functions/index.js
@@ -16,27 +16,33 @@ main.use('/api', app);
 
 exports.webApi = functions.https.onRequest(main);
 
-const collectionName = 'DateTime';
-app.patch('/update/DateTime/:Id', async (req, res) => {
+app.get('/getAll/:collectionName', async (req, res) => {
     try {
-        console.log("********************In try block****************");
-        await firebaseHelper.firestore.updateDocument(db, collectionName, req.params.Id, req.body);
+        await firebaseHelper.firestore.backup(db, req.params.collectionName)
+            .then(data => res.status(200).send(data));
+    } catch (error) {
+        res.status(400).send('Invalid Request!!');
+    }
+});
+
+app.get('/get/:collectionName/:id', async (req, res) => {
+    try {
+        await firebaseHelper.firestore.getDocument(db, req.params.collectionName, req.params.id)
+            .then(doc => res.status(200).send(doc));
+    } catch (error) {
+        res.status(400).send('Invalid Request!!');
+    }
+});
+
+app.patch('/update/:collectionName/:Id', async (req, res) => {
+    try {
+        await firebaseHelper.firestore.updateDocument(db, req.params.collectionName, req.params.Id, req.body);
         res.status(200).send('Document Updated Successfully');
     } catch (error) {
         res.status(400).send('Invalid Request!!');
     }
 });
 
-app.get('/get/DateTime/:id', async (req, res) => {
-    try {
-        console.log("********************In try block****************");
-        const body = await firebaseHelper.firestore.getDocument(db, collectionName, req.params.id);
-        const resBody = JSON.stringify(body);
-        res.status(200).send(resBody);
-    } catch (error) {
-        res.status(400).send('Invalid Request!!');
-    }
-});
 
 exports.helloWorld = functions.firestore.document('DateTime/{Id}').onCreate((snapshot, context) => {
     msgData = snapshot.data();
